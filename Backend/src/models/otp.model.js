c9b207c3ae1c2071f70f767cf75abce81a3113eb
aman23onymous <157746957+aman23onymous@users.noreply.github.com>
2025-07-01 08:03:02 +0000

    nj

--- Backend/src/models/otp.model.js
+++ Backend/src/models/otp.model.js
@@ -4,7 +4,11 @@ import mongoose from "mongoose";
 const otpSchema = new mongoose.Schema({
     user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
     otp: { type: String, required: true },
-    purpose: { type: String, enum: ["transaction"], required: true },
+    purpose: {
+  type: String,
+  enum: ["register", "login", "transaction"],
+  required: true
+},
     createdAt: { type: Date, default: Date.now, expires: 300 } // auto-delete after 5 min
 });
 
